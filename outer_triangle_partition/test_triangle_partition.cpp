ee8fc6d9a4a3cbe376834f01f27486df0c03f622
Allen Mi <allen.mi@yale.edu>
2018-12-08 14:39:40 -0500

    Add benchmarking to outer triangle partition

--- outer_triangle_partition/test_triangle_partition.cpp
+++ outer_triangle_partition/test_triangle_partition.cpp
@@ -10,7 +10,18 @@
 #include <random>
 #include <algorithm>
 
-#define NUM_PTS 132
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+#include <chrono>
+
+#define NUM_PTS (10000)
+#define GEN_PTS (1)
+#define PTS_FILE ("pts_file.csv")
+#define PERF_FILE ("perf_file.csv")
 
 dPoint2 points[NUM_PTS];
 dPoint2 hull[NUM_PTS + 2]; // Add some padding for extra elements
@@ -58,31 +69,73 @@ int main(int argc, const char ** argv) {
   }
   
 #endif // WITH_HCONLIB
-  
+
+if (GEN_PTS) {
+  // random points generation
   for(int i = 0; i < NUM_PTS; i++ ){
     points[i] = dPoint2((rand() % 23113) ^ 1321,
 			(rand() % 34082) ^ 1443);
+  }
+  
+  // output points to file
+  std::ofstream pts_file;
+  pts_file.open(PTS_FILE);
+  for (int i = 0; i < NUM_PTS; i++){
+    pts_file << points[i].x << "," << points[i].y << std::endl;
+  }
+  pts_file.close();
+}
+
+  // read points from file
+  std::string line;
+  std::ifstream pts_file PTS_FILE;
+  if (pts_file.is_open()) {
+    for (int i = 0; i < NUM_PTS; i++){
+      if (getline(pts_file, line)) {
+        std::stringstream ss(line);
+        std::vector<double> pnt;
+        while(ss.good()) {
+          std::string coord;
+          getline(ss, coord, ',');
+          pnt.push_back(std::stod(coord));
+        }
+        points[i] = dPoint2(pnt[0], pnt[1]);
+      } else {
+        std::cout << "Error encountered while reading " << PTS_FILE << std::endl;
+      }
+    }
+    pts_file.close();
+  } else {
+    std::cout << "Unable to open " << PTS_FILE << "\n";
   }
 
   compute_max_dims();
 
   int hullSize = 0;
+
+  auto convex_hull_start = std::chrono::high_resolution_clock::now();
   convex_hull(points, NUM_PTS,
 	      hull, &hullSize);
+  auto convex_hull_stop = std::chrono::high_resolution_clock::now();
+  auto convex_hull_duration = std::chrono::duration_cast<std::chrono::microseconds>
+        (convex_hull_stop - convex_hull_start);
 
 #if WITH_HCONLIB
   setupVisualization(&win, &vas,
 		     width, height);
 #endif // WITH_HCONLIB
 
-
   std::vector<std::vector<dPoint2> > sets;
+
+  auto outer_triangle_start = std::chrono::high_resolution_clock::now();
   outer_triangle_partition(points, NUM_PTS,
-			   hull, hullSize,
-			   sets);
-  
+			  hull, hullSize,
+			  sets);
+  auto outer_triangle_stop = std::chrono::high_resolution_clock::now();
+  auto outer_triangle_duration = std::chrono::duration_cast<std::chrono::microseconds>
+        (outer_triangle_stop - outer_triangle_start);
+
 #if WITH_HCONLIB
-  
   
   for(int i = 0; i < NUM_PTS; i++) {
     vas[screenY(points[i].y) * width + screenX(points[i].x)] = 0x00FF00;
@@ -103,5 +156,16 @@ int main(int argc, const char ** argv) {
   }
 
 #endif // WITH_HCONLIB
+
+  // write and print performance information
+  std::ofstream perf_file;
+  perf_file.open(PERF_FILE);
+  perf_file << "convex_hull" << "," << convex_hull_duration.count() << std::endl;
+  perf_file << "outer_triangle_partition" << "," << outer_triangle_duration.count() << std::endl;
+  perf_file.close();
+
+
+  std::cout << "Time: Convex hull: " << convex_hull_duration.count() << " microseconds" << std::endl;
+  std::cout << "Time: Outer triangle partition: " << outer_triangle_duration.count() << " microseconds" << std::endl;
   
 }
